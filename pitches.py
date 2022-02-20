7bc2a1a05f49b53398f0fb8686c8de5d177cde75
gmonu <gmonu1999@gmail.com>
2022-02-20 09:55:18 +0530

    printing all values in particular column is implemented

--- pitches.py
+++ pitches.py
@@ -1,22 +1,24 @@
-import pandas as pd
 from pyspark.sql import SparkSession
 spark = SparkSession \
     .builder \
     .getOrCreate()
 context = spark.sparkContext
 
+# Data Loading into dataframe
 data = "ShartankIndiaAllPitches.csv"
 df1 = spark.read.format("csv").option("header", "true").load(data)
+
+# Making a table using dataset for querying on that table
 df1.createOrReplaceTempView("sharksdetails")
 column_names = spark.sql("DESCRIBE sharksdetails")
 column_names.show()
 
 # column counts
 print("Total columns = ", column_names.count())
-print(type(column_names))
-# printing total values in particular column
+
+# printing all values in particular column
 for col in column_names.collect():
     col_names = col['col_name']
-    col_values = spark.sql("SELECT '{}' FROM sharksdetails".format(col_names))
-    col_values.show()
+    h = spark.sql(f"Select {col_names} from sharksdetails")
+    h.show()
 
